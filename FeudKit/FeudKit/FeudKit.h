ce5518ed0379b7b402b2862962815ae46a4c97ef
Cameron Slash <calexslash@aol.com>
2023-11-26 00:34:46 -0500

    Initial Commit

---
+++ FeudKit/FeudKit/FeudKit.h
@@ -0,0 +1,18 @@
+//
+//  FeudKit.h
+//  FeudKit
+//
+//  Created by Cameron Slash on 11/26/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeudKit.
+FOUNDATION_EXPORT double FeudKitVersionNumber;
+
+//! Project version string for FeudKit.
+FOUNDATION_EXPORT const unsigned char FeudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeudKit/PublicHeader.h>
+
+
