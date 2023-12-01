6c6eb733149c6f22ec5b647e22d356fdd10b1399
Cameron Slash <calexslash@aol.com>
2023-12-01 00:15:57 -0500

    Added desktop and mobile apps

---
+++ FamilyFeud-Desktop/FamilyFeud-Desktop/FamilyFeud_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyFeud_DesktopApp.swift
+//  FamilyFeud-Desktop
+//
+//  Created by Cameron Slash on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FamilyFeud_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
